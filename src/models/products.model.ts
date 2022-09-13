a891a52e5048e1096f7f24c20aeee221152a4c37
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-09-13 18:26:58 -0300

    Criação do endpoint de cadastro de produtos (req01)

---
+++ src/models/products.model.ts
@@ -0,0 +1,25 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../interfaces/product.interface';
+
+class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+
+    return { id: insertId, ...product };
+  }
+}
+
+export default ProductModel;
---
+++ src/services/products.service.ts
@@ -0,0 +1,18 @@
+import connection from '../models/connection';
+import ProductModel from '../models/products.model';
+import Product from '../interfaces/product.interface';
+
+class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const result = await this.model.create(product);
+    return result;
+  }
+}
+
+export default ProductService;
