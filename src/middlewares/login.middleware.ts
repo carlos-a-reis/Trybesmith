0306bbd0bf667874d634e97692b051824dfc8e0c
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-09-14 16:28:58 -0300

    Refatoramento do middleware de validação dos campos de login para uma versão mais simplificada com o uso de Joi

--- src/middlewares/login.middleware.ts
+++ src/middlewares/login.middleware.ts
@@ -1,25 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import Joi from 'joi';
 import Login from '../interfaces/login.interface';
 
-const properties = ['username', 'password'];
-
-function validateProperties(login: Login): [boolean, string | null] {
-  for (let i = 0; i < properties.length; i += 1) {
-    if (!Object.prototype.hasOwnProperty.call(login, properties[i])) {
-      return [false, properties[i]];
-    }
-  }
-  return [true, null];
-}
+const schemaLogin = Joi.object({
+  username: Joi.string().min(2).required().messages({
+    'string.required': '"username" is required',
+  }),
+  password: Joi.string().min(2).required().messages({
+    'string.required': '"password" is required',
+  }),
+});
 
 function validationLogin(req: Request, res: Response, next: NextFunction) {
   const login: Login = req.body;
 
-  const [valid, property] = validateProperties(login);
+  const { error } = schemaLogin.validate(login);
 
-  if (!valid) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: `"${property}" is required` });
+  if (error) {
+    const { message } = error.details[0];
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ message });
   }
 
   next();
